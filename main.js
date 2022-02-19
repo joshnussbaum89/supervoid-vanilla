802dcc74f2f7f64a252a9c02f3b15ebbfa502f97
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2022-02-19 13:29:04 -0600

    mobile nav functionality + styling, refactor

--- main.js
+++ main.js
@@ -1,14 +1,56 @@
 // Global DOM selectors
 const hamburgerMenu = document.querySelector('.hamburger')
+const mobileNavItems = document.querySelectorAll('.mobile-nav a')
+const projectContainer = document.querySelectorAll('.project-container')
+let projectData
+
+// Load all project data + assign to projectData variable to be used anywhere onsite
+async function getProjectData() {
+  let response = await fetch('./assets/data.json')
+  let data = await response.json()
+  projectData = data
+}
 
 // Mobile > toggle navigation menu
 function toggleHamburgerMenu() {
-  const hamburgerMenu = document.querySelector('.hamburger')
   const mobileNav = document.querySelector('.mobile-nav')
 
+  // Toggle active classes for styling
   hamburgerMenu.classList.toggle('active')
   mobileNav.classList.toggle('active')
 }
 
+// Hide mobile navigation on desktop
+function hideMobileNavOnDesktop() {
+  if (window.innerWidth >= 768) {
+    const mobileNav = document.querySelector('.mobile-nav')
+
+    // Remove all active classes
+    hamburgerMenu.classList.remove('active')
+    mobileNav.classList.remove('active')
+  }
+}
+
+// Open project information modal
+function openProjectModal() {
+  console.log('project clicked...')
+}
+
+// Page load > project JSON
+window.addEventListener('load', getProjectData)
+
+// Window resize > hide mobile navigation on desktop
+window.addEventListener('resize', hideMobileNavOnDesktop)
+
 // Mobile > user clicks hambruger menu > toggle menu open
 hamburgerMenu.addEventListener('click', toggleHamburgerMenu)
+
+// Mobile > user clicks hamburger menu > toggle menu open
+mobileNavItems.forEach((item) =>
+  item.addEventListener('click', toggleHamburgerMenu)
+)
+
+// User clicks project > open modal with more information
+projectContainer.forEach((project) =>
+  project.addEventListener('click', openProjectModal)
+)
