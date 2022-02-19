89ff9a0a2b7feaf6d78d8cddf88debc22a26e65b
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2022-02-19 11:04:01 -0600

    remove jQuery, build new hamburger icon

--- main.js
+++ main.js
@@ -1,25 +1,10 @@
+// Global DOM selectors
+const hamburgerMenu = document.querySelector('.hamburger')
+
 // Mobile > toggle navigation menu
 function toggleHamburgerMenu() {
   this.classList.toggle('is-active')
 }
 
-// Smooth scroll to specifc location on page
-function smoothScroll(location, duration) {
-  $('html, body').animate(
-    { scrollTop: $(location).offset().top - 100 },
-    duration
-  )
-}
-
-// Scroll to portfolio
-function scrollToPortfolio() {
-  $(this).on('click', function () {
-    smoothScroll('#portfolio', 200)
-  })
-}
-
-// User clicks portfolio CTA > navigate to #portfolio section
-$('.portfolio-cta--js').each(scrollToPortfolio)
-
 // Mobile > user clicks hambruger menu > toggle menu open
-$('.hamburger').on('click', toggleHamburgerMenu)
+hamburgerMenu.addEventListener('click', toggleHamburgerMenu)
